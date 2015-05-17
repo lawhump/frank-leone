329de8143669a5d1780926f5d05fa256bbd0cfc5
Lawrence Humphrey <lawrencehumphrey@Lawrences-MacBook-Pro-2.local>
2015-05-17 18:46:17 -0500

    responsive perhaps

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+$(window).resize(function(){
+   console.log('resize called');
+   var width = $(window).width();
+   if(width <= 768){
+       $('#tellem').removeClass('vis').addClass('invis');
+   }
+   else{
+       $('#tellem').removeClass('invis').addClass('vis');
+   }
+})
+.resize();
 
+$(document).ready(function () {
+    $('div.header').fadeIn(2500).removeClass('hidden');
+    $('div.links').delay(2200).fadeIn(800).removeClass('hidden');
+    $('div.enterwild').delay(3400).fadeIn(800).removeClass('hidden');
+});
