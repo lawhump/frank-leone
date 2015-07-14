e891fc5023c0a4321182b50425a8a593971a62cc
Lawrence Humphrey <lawrenceh1993@gmail.com>
2015-07-14 00:09:06 -0500

    slight rework; not as much aesthetically, but the code is less terrible

--- js/main.js
+++ js/main.js
@@ -13,19 +13,41 @@ $(window).resize(function(){
         $('.sc-wrapper').removeClass('vis').addClass('invis');
     }
 
-    if(width <= 580){
+    if(width <= 650){
         // split email
-        $("section.four a h3").html("LEONEMUSICAL @GMAIL.COM");
+        $(".four a h4").html("LEONEMUSICAL @GMAIL.COM");
     }
     else{
         // make email one line
-        $("section.four a h3").html("LEONEMUSICAL@GMAIL.COM");
+        $(".four a h4").html("LEONEMUSICAL@GMAIL.COM");
     }
 })
 .resize();
+
+function initGrid() {
+    imagesLoaded('.grid', function() {
+        // init Masonry
+        var $grid = $('.grid').masonry({
+            // options
+            itemSelector: '.grid-item',
+            isFitWidth: true,
+            columnWidth: '.grid-sizer',
+            gutter: 10
+        });
+
+        // $grid.on( 'click', '.grid-item', function() {
+        //     // change size of item via class
+        //     $( this ).toggleClass('grid-item--');
+        //     // trigger layout
+        //     $grid.masonry();
+        // });
+    });
+}
 
 $(document).ready(function () {
     $('div.header').fadeIn(2500).removeClass('hidden');
     $('div.links').delay(2200).fadeIn(800).removeClass('hidden');
     $('div.enterwild').delay(3400).fadeIn(800).removeClass('hidden');
+
+    initGrid();
 });
