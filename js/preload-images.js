555196d06d57b0f8594a82f8d3f0622ed2f9932d
Lawrence Humphrey <lawrenceh1993@gmail.com>
2015-07-21 14:07:38 -0500

    preloading images probably works

---
+++ js/preload-images.js
@@ -0,0 +1,19 @@
+var $container = $('.three');
+
+$container.imagesLoaded()
+  .always( function( instance ) {
+    console.log('all images loaded');
+  })
+  .done( function( instance ) {
+    console.log('all images successfully loaded');
+  })
+  .fail( function() {
+    console.log('all images loaded, at least one is broken');
+  })
+  .progress( function( instance, image ) {
+    var result = image.isLoaded ? 'loaded' : 'broken';
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+
+    var $item = $( image.img ).parents('.grid-item');
+    $item.removeClass('is-loading');
+});
