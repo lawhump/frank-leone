aec30fc76c3544b78e4611f4149548eed6bb54ef
Lawrence Humphrey <lawrenceh1993@gmail.com>
2015-07-28 15:43:16 -0500

    performance and production things

---
+++ dist/js/all.js
@@ -0,0 +1,82 @@
+window.onresize = function() {
+    var width = window.innerWidth;
+
+    if(width <= 420){
+        // split email
+        document.querySelector(".four a h3").innerHTML("LEONEMUSICAL @GMAIL.COM");
+    }
+    else{
+        // make email one line
+        document.querySelector(".four a h3").innerHTML("LEONEMUSICAL@GMAIL.COM");
+    }
+};
+
+function initGrid() {
+    imagesLoaded('.grid', function() {
+        var elem = document.querySelector('.grid');
+        var msnry = new Masonry( elem, {
+            // options
+            itemSelector: '.grid-item',
+            isFitWidth: true,
+            columnWidth: '.grid-sizer',
+            gutter: 10
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    initGrid();
+});
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+var container = imagesLoaded('.three');
+
+function getParent(el) {
+    var p = el.parentNode;
+
+    while (!p.className.includes('grid-item')) {
+        p = p.parentNode;
+    }
+    return p;
+}
+
+container.on( 'always', function( instance ) {
+    // console.log('all images loaded');
+});
+container.on( 'done', function( instance ) {
+    // console.log('all images successfully loaded');
+});
+container.on( 'fail', function() {
+    // console.log('all images loaded, at least one is broken');
+});
+container.on( 'progress', function( instance, image ) {
+  var result = image.isLoaded ? 'loaded' : 'broken';
+  // console.log( 'image is ' + result + ' for ' + image.img.src );
+
+  var item = getParent( image.img );
+  item.classList.remove('is-loading');
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var sass = require('gulp-sass');
+var concatCSS = require('gulp-concat-css');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    gulp.src('css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('styles', function() {
+    gulp.src(['css/normalize.css', 'css/main.css'])
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(minifyCSS())
+        .pipe(rename('all.min.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['scripts']);
+    gulp.watch('css/*.scss', ['sass', 'styles']);
+});
+
+// Default Task
+gulp.task('default', ['sass', 'scripts', 'styles', 'watch']);
